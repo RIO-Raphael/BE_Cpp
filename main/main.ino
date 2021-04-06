df002084eaaebfdf21043dc1445f5e96c8e423ed
RIO <raphael.06rio@gmail.com>
2021-04-06 15:57:37 +0200

    template

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,18 @@
+#include "header.h"
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(LED_BUILTIN,OUTPUT);
+  pinMode(LED_BUILTIN_AUX,OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  digitalWrite(LED_BUILTIN,1);
+  digitalWrite(LED_BUILTIN_AUX,0);
+  delay (1000);
+  digitalWrite(LED_BUILTIN,0);
+  digitalWrite(LED_BUILTIN_AUX,1);
+  delay (1000);
 }
