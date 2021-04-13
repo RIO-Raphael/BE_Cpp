4db4d663440fa955259e398bb17ec088a85c7eba
MathildeIbled <mathildeibled59@orange.fr>
2021-04-13 13:12:27 +0200

    Definition des classes

--- main/header.h
+++ main/header.h
@@ -1,2 +1,47 @@
-#define LED_BUILTIN 2
-#define LED_BUILTIN_AUX 16
+//#define LED_BUILTIN 2
+//#define LED_BUILTIN_AUX 16
+
+template <class T> class Capteur {
+  
+  public :
+  //Constructeur
+  Capteur();
+  //Destructeur
+  //~Capteur();
+  //Fonction
+  virtual T get_value(void)=0; 
+  private :
+  
+};
+
+template <class V> class Ultrason : public class Capteur<V> {
+  public :
+  //Constructeur
+  Ultrason();
+  //Destructeur
+  //~Capteur();
+  //Fonction
+  virtual int get_value(); 
+  private :
+  
+};
+
+/*class Mouvement : public class Capteur<float> {
+  public :
+  //Constructeur
+  Mouvement();
+  //Destructeur
+  //~Capteur();
+  //Fonction
+  virtual float get_value(void); 
+  private :
+};*/
+
+template <class Y> class Actionneur {
+  //Constructeur
+  Actionneur();
+
+  //Fonction
+  virtual Y set_value(void)=0;
+  
+};
--- main/main.ino
+++ main/main.ino
@@ -10,38 +10,11 @@ int state = 1;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED_BUILTIN,OUTPUT);
-  pinMode(LED_BUILTIN_AUX,OUTPUT);
-
-  //temp
-  Serial.begin(BAUDRATE);
+ 
 
 }
 
 void loop() {
-  if (!TEMP){
-    // put your main code here, to run repeatedly:
-    digitalWrite(LED_BUILTIN,1);
-    digitalWrite(LED_BUILTIN_AUX,0);
-    delay (1000);
-    digitalWrite(LED_BUILTIN,0);
-    digitalWrite(LED_BUILTIN_AUX,1);
-    delay (1000);
-  }else{
-    state++;
-    int a = analogRead(pinTempSensor);
   
-    float R = 1023.0/a-1.0;
-    R = R0*R;
-    
-    float temperature = 1.0/(log(R/R0)/B+1/298.15)-273.15; // convert to temperature via datasheet
-  
-    Serial.print("T° = ");
-    Serial.print(temperature);
-    Serial.print("°C");
-    Serial.print("\n\r");
-    digitalWrite(LED_BUILTIN,state%2);
-  
-    delay(10000);
-  }
+ 
 }
